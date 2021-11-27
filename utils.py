f13799535f4158739cd9cc9e0f55e4d7566ea2b6
Luca <luca.rauchenberger@gmail.com>
2021-11-27 09:33:36 +0100

    put Counter into utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import time
+from multiprocessing import RawValue, Lock
 import json
 from hashlib import md5
 
@@ -9,3 +9,22 @@ def hash_dict(dct):
     dct['keywords'].sort()
     dct_dumped = json.dumps(dct, sort_keys=True).encode('utf-8')
     return md5(dct_dumped).hexdigest()[0:7]
+
+
+class Counter(object):
+    def __init__(self, value=0):
+        # RawValue because we don't need it to create a Lock:
+        self.val = RawValue('i', value)
+        self.lock = Lock()
+
+    def increment(self):
+        with self.lock:
+            self.val.value += 1
+
+    def decrement(self):
+        with self.lock:
+            self.val.value -= 1
+
+    def value(self):
+        with self.lock:
+            return self.val.value

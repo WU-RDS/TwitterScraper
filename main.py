1563c6afa63b64a9f0afe80eeb80917dac97772b
Luca <luca.rauchenberger@gmail.com>
2021-11-27 20:21:57 +0100

    removed comment

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from worker import dispatch_workers
 from multiprocessing import Queue
 from utils import Counter
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     q = Queue()
     counter = Counter(0)
